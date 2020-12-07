326aa6a6e5559850e8ac8217bd512940ca401352
Marlyza <maria.lyzzie@gmail.com>
2020-12-07 12:29:42 +0100

    Add base64 encode/decode function

---
+++ mtools.h
@@ -0,0 +1,41 @@
+/* MiniDLNA media server
+ * Copyright (C) 2008-2017  Justin Maggard
+ *
+ * This file is part of MiniDLNA.
+ *
+ * MiniDLNA is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
+ *
+ * MiniDLNA is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with MiniDLNA. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+ /* Set of tools */
+
+#ifndef MTOOLS_H
+#define MTOOLS_H 1
+
+
+/* Bases64 encoding/decoding tools */
+
+static const char encoding_table[] = {  'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H',
+                                        'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P',
+                                        'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X',
+                                        'Y', 'Z', 'a', 'b', 'c', 'd', 'e', 'f',
+                                        'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n',
+                                        'o', 'p', 'q', 'r', 's', 't', 'u', 'v',
+                                        'w', 'x', 'y', 'z', '0', '1', '2', '3',
+                                        '4', '5', '6', '7', '8', '9', '+', '/'};
+static const int mod_table[] = {0, 2, 1};
+
+char *base64_encode(const char *data,   size_t input_length, size_t *output_length);
+unsigned char *base64_decode(const char *data, size_t input_length,   size_t *output_length );
+
+
+#endif
